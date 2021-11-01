95ba26929bf42e0f1b1a812b94332688428bfae3
Fátima Rojas <64380871+Fatima2427@users.noreply.github.com>
2021-10-31 20:07:20 -0500

    q5 es o(n^2)

--- src/timeComplexity/q3Ejercicio.java
+++ src/timeComplexity/q3Ejercicio.java
@@ -1,11 +1,12 @@
-public class q2Ejercicio {
+
+public class q3Ejercicio {
 	public static void main (String [] args) {
 	int [] lista = {4,7,8,5,9,4,1,7,0,5,8,4,9};
-	q2(lista);
+	q3(lista);
 	}
-	public static void q2 ( int [] lista) {//O(n)
+	public static void q3 ( int [] lista) {//O(n)
 		int n= lista.length;
-	for ( int i= n ; i>0 ; i--) {
+	for ( int i= 0; i<n ; i=i+2) {
 			System.out.println(i);
 		}
 	}
--- src/timeComplexity/q5Ejercicio.java
+++ src/timeComplexity/q5Ejercicio.java
@@ -1,13 +1,13 @@
 
-public class q4Ejercicio {
+public class q5Ejercicio {
 	public static void main (String [] args) {
 	int [] lista = {4,7,8,5,9,4,1,7,0,5,8,4,9};
-	q4(lista);
+	q5(lista);
 	}
-	public static void q4 ( int [] lista) {//O(n'2)
+	public static void q5 ( int [] lista) {//O(n'2)
 		int n= lista.length;
 	for ( int i= 0; i<n ; i++) {
-		for ( int j= 0; j<n ; j++) {
+		for ( int j= 0; j<i; j++) {
 			System.out.println("j " + j);
 		}
 		System.out.println("i--> "  + i);
