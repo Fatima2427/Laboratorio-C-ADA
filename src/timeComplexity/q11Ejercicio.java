5f5ad0be9eca79ad0d1ddd9c7ae313e6baa48639
Fátima Rojas <64380871+Fatima2427@users.noreply.github.com>
2021-11-01 22:28:43 -0500

    Update q11Ejercicio.java

--- src/timeComplexity/q11Ejercicio.java
+++ src/timeComplexity/q11Ejercicio.java
@@ -3,7 +3,7 @@ public class q11Ejercicio {
 	public static void main (String [] args) {
 	q11(100);
 	}
-	public static void q11 ( int n) {//O(n'2+m'2)
+	public static void q11 ( int n) {//O(log2n+log2m)
 
 	for ( int i= 1; i<n ;i=i*2) {
 			System.out.println("j " + i);
