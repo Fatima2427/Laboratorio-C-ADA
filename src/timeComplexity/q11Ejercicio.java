04d1e98db00dc6972d4b248208fedd783684a245
Fátima Rojas <64380871+Fatima2427@users.noreply.github.com>
2021-11-01 21:23:56 -0500

    11, 12 complejidad
    doc aparte sobre por que tienen esa complejidad

--- src/timeComplexity/q11Ejercicio.java
+++ src/timeComplexity/q11Ejercicio.java
@@ -1,11 +1,15 @@
-public class q9Ejercicio {
-	public static void main (String [] args) {
-	q9(100);
-	}
-	public static void q9 ( int n) {//Olog(n)
 
-	for ( int i= 0; i*i<=n ;i++) {
+public class q11Ejercicio {
+	public static void main (String [] args) {
+	q11(100);
+	}
+	public static void q11 ( int n) {//O(n'2+m'2)
+
+	for ( int i= 1; i<n ;i=i*2) {
 			System.out.println("j " + i);
 		}
+	for ( int j= 1; j<=n ;j=j*2) {
+		System.out.println("j " + j);
+	}
 	}
 }
--- src/timeComplexity/q12Ejercicio.java
+++ src/timeComplexity/q12Ejercicio.java
@@ -1,11 +1,14 @@
-public class q9Ejercicio {
-	public static void main (String [] args) {
-	q9(100);
-	}
-	public static void q9 ( int n) {//Olog(n)
 
-	for ( int i= 0; i*i<=n ;i++) {
-			System.out.println("j " + i);
+public class q12Ejercicio {
+	public static void main (String [] args) {
+	q12(100);
+	}
+	public static void q12 ( int n) {//O(nlog2n)
+	for ( int i= 0; i<n ; i++) {
+		for ( int j= 1; j<n; j=j*2) {
+			System.out.println("j " + j);
+		}
+		System.out.println("i--> "  + i);
 		}
 	}
 }
