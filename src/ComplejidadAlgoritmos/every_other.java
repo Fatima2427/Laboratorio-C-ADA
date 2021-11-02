04970397cfad17b8849f6a6422623569ab507ac1
Fátima Rojas <64380871+Fatima2427@users.noreply.github.com>
2021-11-02 16:39:55 -0500

    Update every_other.java

--- src/ComplejidadAlgoritmos/every_other.java
+++ src/ComplejidadAlgoritmos/every_other.java
@@ -7,10 +7,10 @@ public class every_other {
 	// what is the time complexity of this code?
 	public static void everyOther ( int [] list) {//O(n'2)
 
-	for ( int i =0;i <list.length;i++) {
-		if ( i %2 ==0) {
-			for ( int j : list) {
-				System.out.println( list[i]+j);
+	for ( int i =0;i <list.length;i++) {//o(2n*n)
+		if ( i %2 ==0) {//o(2n)
+			for ( int j : list) {//o(n)
+				System.out.println( list[i]+j); //1
 			}
 			}
 		}
