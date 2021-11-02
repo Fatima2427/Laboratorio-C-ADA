f46e2e8cddc47eb92243d97a8addb61017d50288
Fátima Rojas <64380871+Fatima2427@users.noreply.github.com>
2021-11-01 22:16:46 -0500

    arreglo time de q9

--- src/timeComplexity/q9Ejercicio.java
+++ src/timeComplexity/q9Ejercicio.java
@@ -2,7 +2,7 @@ public class q9Ejercicio {
 	public static void main (String [] args) {
 	q9(100);
 	}
-	public static void q9 ( int n) {//Olog(n)
+	public static void q9 ( int n) {//O(n)
 
 	for ( int i= 0; i*i<=n ;i++) {
 			System.out.println("j " + i);
