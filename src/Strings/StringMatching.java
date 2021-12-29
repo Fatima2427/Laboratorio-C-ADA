c2a47038b685fa0fb68b2cf974bbf7ad27a1b337
Fátima Rojas <64380871+Fatima2427@users.noreply.github.com>
2021-12-29 01:19:38 -0500

    ----

--- src/Strings/StringMatching.java
+++ src/Strings/StringMatching.java
@@ -18,13 +18,14 @@ public class Matching{
 						if( text.charAt(i)==pattern.charAt(0)) {
 							String cadena = text.substring(i,i+pattern.length());
 							if(cadena.equals(pattern))
+
 								rpta.append(i).append(' ');
 						
 	    	  
 	      }
 		}
 			}
-				rpta.append(" n");
+				rpta.append("\n");
 			}
 				 System.out.println(rpta);
 			
@@ -33,3 +34,42 @@ public class Matching{
 	    
 	
 }
+/*
+
+package Strings;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Matching{
+	public static void main (String [] args)throws Exception {
+		Scanner sc = new Scanner(System.in);
+		ArrayList<String> list = new ArrayList<String>();
+
+		//while (sc.hasNextLine()) {
+			String pattern = "p";
+			String text ="Popup";
+			pattern = "p";
+			 text ="Poup";
+
+		StringBuilder rpta = new StringBuilder();
+			int tamaño=text.length();
+			if( pattern!=null && text !=null){
+				if ( text.length() <pattern.length()) {
+					rpta.append("");
+				}
+					for(int i=0;i<tamaño;i++){
+						if( text.charAt(i)==pattern.charAt(0)) {
+							String cadena = text.substring(i,i+pattern.length());
+							if(cadena.equals(pattern))
+								rpta.append(i).append(' ');
+						
+	    	  
+	      }
+		//}
+			}
+				rpta.append("");
+				list.add(rpta.toString());
+			}
+				 System.out.println(list);
+			}
+		}
+		*/
